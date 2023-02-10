9993871b358276b398513d4f28f228328313c773
Juan Camilo Villada <jvillad1@gmail.com>
2023-02-10 00:13:31 -0500

    feature: send events from congrats module and handle in background on app module

--- congrats/src/main/java/com/jvillad1/congrats/MainActivity2.kt
+++ congrats/src/main/java/com/jvillad1/congrats/MainActivity2.kt
@@ -1,8 +1,6 @@
-package com.jvillad1.playground
+package com.jvillad1.congrats
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -14,59 +12,61 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.jvillad1.congrats.MainActivity2
 import com.jvillad1.congrats.events.PaymentEventHandler
 import com.jvillad1.congrats.events.PaymentResultEvent
 import com.jvillad1.congrats.events.PerformPaymentEvent
-import com.jvillad1.playground.ui.theme.MyApplicationTheme
+import com.jvillad1.congrats.ui.theme.CongratsTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity2 : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MyApplicationTheme {
+            CongratsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    PaymentEventScreen(
+                        onPerformPaymentEvent = {
+                            PaymentEventHandler.publishPaymentEvent(
+                                this,
+                                PerformPaymentEvent(paymentData = "Payment Data")
+                            )
+                        }
+                    ) {
+                        PaymentEventHandler.publishPaymentEvent(
+                            this,
+                            PaymentResultEvent(paymentResultData = "Payment result Data")
+                        )
+                    }
                 }
-            }
-        }
-
-        PaymentEventHandler.subscribePaymentEvent(this) { paymentEvent ->
-            when (paymentEvent) {
-                is PerformPaymentEvent ->
-                    Log.d(
-                        "PaymentEventHandler",
-                        "${paymentEvent.paymentData} PerformPaymentEvent"
-                    )
-
-                is PaymentResultEvent ->
-                    Log.d(
-                        "PaymentEventHandler",
-                        "${paymentEvent.paymentResultData} PaymentResultEvent"
-                    )
             }
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    val context = LocalContext.current
-
+fun PaymentEventScreen(
+    onPerformPaymentEvent: () -> Unit = {},
+    onPaymentResultEvent: () -> Unit = {}
+) {
     Column {
         Row {
             Button(
-                onClick = { context.startActivity(Intent(context, MainActivity2::class.java)) }
+                onClick = onPerformPaymentEvent
             ) {
-                Text(text = "Navigate to Library Flow")
+                Text(text = "Publish PerformPaymentEvent")
+            }
+        }
+        Row {
+            Button(
+                onClick = onPaymentResultEvent
+            ) {
+                Text(text = "Publish PaymentResultEvent")
             }
         }
     }
@@ -74,8 +74,8 @@ fun MainScreen() {
 
 @Preview(showBackground = true)
 @Composable
-fun MainPreview() {
-    MyApplicationTheme {
-        MainScreen()
+fun GreetingPreview() {
+    CongratsTheme {
+        PaymentEventScreen()
     }
 }
