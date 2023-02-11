9b3d6f50f73938de7ae9e7ae675ca6f9afbb72b5
Juan Vilada <jvillad1@gmail.com>
2023-02-11 15:51:14 -0500

    feature: playing with Layouts and Modifiers

--- aliens/src/main/java/com/jvillad1/aliens/AliensActivity.kt
+++ aliens/src/main/java/com/jvillad1/aliens/AliensActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -48,19 +53,61 @@ fun AndroidAlien(
 }
 
 @Composable
+fun AndroidAliensRow() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Top,
+    ) {
+        AndroidAlien(
+            color = Color.Red,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+        AndroidAlien(
+            color = Color.Green,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1F)
+                .padding(4.dp)
+        )
+        AndroidAlien(
+            color = Color.Blue,
+            modifier = Modifier
+                .align(CenterVertically)
+                .size(70.dp)
+                .padding(4.dp)
+        )
+    }
+}
+
+@Composable
+fun AndroidAliensColumn() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        AndroidAlien(
+            color = Color.Red,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+        AndroidAlien(
+            color = Color.Green,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+    }
+}
+
+@Composable
 fun AndroidAliens() {
-    AndroidAlien(
-        color = Color.Red,
-        modifier = Modifier
-            .size(70.dp)
-            .padding(4.dp)
-    )
-    AndroidAlien(
-        color = Color.Green,
-        modifier = Modifier
-            .size(70.dp)
-            .padding(4.dp)
-    )
+    AndroidAliensRow()
+//    AndroidAliensColumn()
 }
 
 @Preview(showBackground = true)
